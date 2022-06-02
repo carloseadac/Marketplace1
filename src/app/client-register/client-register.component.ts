54bda0fa34d835fd167dcc4d6342013f56ea016f
carloseadac <carloseadac@gmail.com>
2022-06-02 16:52:39 -0300

    mais um ou menos um

--- src/app/client-register/client-register.component.ts
+++ src/app/client-register/client-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from "axios";
 
 @Component({
   selector: 'app-client-register',
@@ -12,8 +13,24 @@ export class ClientRegisterComponent implements OnInit {
 
   ngOnInit(): void {
     
-    }
+  }
   
+  save(){
+
+    let name = document.getElementById("nome") as HTMLInputElement;
+    let phone = document.getElementById("tel") as HTMLInputElement;
+    let documento = document.getElementById("document") as HTMLInputElement;
+    let email = document.getElementById("email") as HTMLInputElement;
+    let login = document.getElementById("login") as HTMLInputElement;
+    let passwd = document.getElementById("passwd") as HTMLInputElement;
+    let date_of_birth = document.getElementById("date_of_birth") as HTMLInputElement;
+
+    let street =document.getElementById("rua") as HTMLInputElement;
+    let state =document.getElementById("estado") as HTMLInputElement;
+    let city =document.getElementById("cidade") as HTMLInputElement;
+    let country =document.getElementById("pais") as HTMLInputElement;
+    let postal_code =document.getElementById("cep") as HTMLInputElement;
+  }
   
 
 }
