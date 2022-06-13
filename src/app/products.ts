986ef6409badb72aa7d18c9b7dc350e1bacabe4e
carloseadac <carloseadac@gmail.com>
2022-06-13 16:56:38 -0300

    aaaaaa

--- src/app/products.ts
+++ src/app/products.ts
@@ -6,85 +6,12 @@ export interface Product {
     image: string;
     storeid: number;
     store: string;
+
+    unit_price: number;
+    cnpj:string;
+    idStocks:number;
+    idWishlist:number;
+    quantity:number;
   }
   
-  export const products = [
-    {
-      id: 1,
-      name: 'Phone XL',
-      price: 799,
-      description: 'A large phone with one of the best screens',
-      image: "../assets/iphone.png"
-    },
-    {
-      id: 2,
-      name: 'Phone Mini',
-      price: 699,
-      description: 'A great phone with one of the best cameras',
-      image: "../assets/iphone.png"
-    },
-    {
-      id: 3,
-      name: 'Phone Standard',
-      price: 299,
-      description: '',
-      image: "../assets/iphone.png"
-    },
-    {
-      id: 3,
-      name: 'Phone Standard',
-      price: 299,
-      description: '',
-      image: "../assets/iphone.png"
-    },
-    {
-      id: 3,
-      name: 'Phone Standard',
-      price: 299,
-      description: '',
-      image: "../assets/iphone.png"
-    },
-    {
-      id: 3,
-      name: 'Phone Standard',
-      price: 299,
-      description: '',
-      image: "../assets/iphone.png"
-    },
-    {
-      id: 3,
-      name: 'Phone Standard',
-      price: 299,
-      description: '',
-      image: "../assets/iphone.png"
-    },
-    {
-      id: 3,
-      name: 'Phone Standard',
-      price: 299,
-      description: '',
-      image: "../assets/iphone.png"
-    },
-    {
-      id: 3,
-      name: 'Phone Standard',
-      price: 299,
-      description: '',
-      image: "../assets/iphone.png"
-    },
-    {
-      id: 3,
-      name: 'Phone Standard',
-      price: 299,
-      description: '',
-      image: "../assets/iphone.png"
-    },
-    {
-      id: 3,
-      name: 'Phone Standard',
-      price: 299,
-      description: '',
-      image: "../assets/iphone.png"
-    }
-  ];
-  
+ 
--- src/app/wishlist/wishlist.component.ts
+++ src/app/wishlist/wishlist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../products';
+import axios from 'axios';
 
 @Component({
   selector: 'app-wishlist',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WishlistComponent implements OnInit {
 
-  constructor() { }
+  products : [Product] |undefined;
 
   ngOnInit(): void {
+    this.LoadProducts();
   }
+
+
+  LoadProducts(){
+    var data = JSON.stringify({});
+
+    var config = {
+      method: 'get',
+      url: 'http://localhost:5136/wishlist/getwishlist',
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem("authToken"),
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+    console.log(localStorage.getItem("authToken"))
+    let instance = this
+    axios(config)
+    .then(function (response) {
+      instance.products = response.data
+      console.log(response.data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  RemoveWishList(WishListId:number){
+    console.log("teste")
+  }
+
+
 
 }
